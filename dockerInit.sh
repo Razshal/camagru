c95814a15d879ebf703b6712c727c046247e1b68
Razshal <fontenille.mathieu@gmail.com>
2018-04-24 18:52:36 +0200

    docker config script in progress

--- dockerInit.sh
+++ dockerInit.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 my_sql_password="j'aimeles1576483bonnesmontagnesrectangulaires"
 project_folder=$HOME/Projects/web/camagru
+machine_name="web"
 
-eval $(docker-machine env web)
+if ! docker-machine ls -q | grep $machine_name
+then
+	docker-machine create --driver virtualbox $machine_name
+fi
+
+eval $(docker-machine env $machine_name)
 docker run -dit -p 8888:3306 -e MYSQL_ROOT_PASSWORD=$my_sql_password --name mysql mysql
 docker run -d -p 8100:80 -v $project_folder:/var/www/html --name phpapache php:apache
+
