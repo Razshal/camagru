4425651706f475470ac55b81fbc1bca8e1674d54
Razshal <fontenille.mathieu@gmail.com>
2018-04-24 18:02:01 +0200

    color chart and font familly

---
+++ views/example page.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php include("views/head.php") ?>
+    <body>
+        <?php include("views/header.php") ?>
+        <main>
+            <h2>Welcome To Camagru</h2>
+        </main>
+    </body>
+    <?php include("views/footer.php") ?>
+</html>
--- views/head.php
+++ views/head.php
@@ -2,4 +2,6 @@
     <meta charset="UTF-8">
     <title>Camagru</title>
     <link rel="stylesheet" type="text/css" href="views/style/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Handlee' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
 </head>
