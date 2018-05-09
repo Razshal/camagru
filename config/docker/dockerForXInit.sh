f95629811a2a62c76a5f3d0bbef94914b3c1ce92
Razshal <fontenille.mathieu@gmail.com>
2018-05-09 20:23:11 +0200

    scurity fixes

--- config/docker/dockerForXInit.sh
+++ config/docker/dockerForXInit.sh
@@ -2,5 +2,5 @@
 mysql_password="jaimeles1576483bonnesmontagnesrectangulaires"
 project_folder=$HOME/Projects/web/camagru
 
-docker run -d -p 8889:3306 -e MYSQL_ROOT_PASSWORD=$mysql_password -e MYSQL_DATABASE=camagru --name mysql mysql:5.6
+docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=$mysql_password -e MYSQL_DATABASE=camagru --name mysql mysql:5.6
 docker build -t phpapache . && docker run -d -p 80:80 -p 25:25 -v $project_folder:/var/www/html --name phpapache phpapache
