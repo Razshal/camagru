8ee6e87363a7c62a3195247d6f68074e3562b8a9
Razshal <fontenille.mathieu@gmail.com>
2018-05-11 14:31:20 +0200

    mvc refactor wip

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
-<?php include ($_SERVER["DOCUMENT_ROOT"] . "/views/structure/head.php") ?>
-<html lang="en">
-    <body>
-        <?php include ($_SERVER["DOCUMENT_ROOT"] . "/views/structure/header.php") ?>
-        <main>
-            <h2>Welcome To Camagru</h2>
-        </main>
-    </body>
-    <?php include ($_SERVER["DOCUMENT_ROOT"] . "/views/structure/footer.php") ?>
-</html>
+<?php
+session_start();
+require ("config/database.php");
+require ("config/site.php");
+require ("model/class_database.php");
+
+$title = "Camagru";
+
+try {
+    $database = new Database($DB_DSN, $DB_USER, $DB_PASSWORD, $SITE_ADDRESS);
+    if (isset($_SESSION) && isset($_SESSION["user"]) && $_SESSION["user"] != "")
+        if (empty($database->get_user($_SESSION["user"])))
+            $_SESSION["user"] = "";
+} catch (Exception $e) {
+    $database = NULL;
+    echo "<h1 class='error'>Fatal database error</h1>";
+}
+
+$content = "<h2>Welcome To Camagru</h2>";
+
+require ("views/structure/template.php");
--- views/structure/template.php
+++ views/structure/template.php
@@ -1,9 +1,8 @@
-<?php require_once ($_SERVER["DOCUMENT_ROOT"] . "/controller/C_head.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>Camagru</title>
+        <title><?= $title ?></title>
         <link rel="stylesheet" type="text/css" href="/views/style/style.css">
         <link href='https://fonts.googleapis.com/css?family=Handlee' rel='stylesheet'>
         <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
@@ -23,10 +22,9 @@
         </header>
         <main>
             <?= $content ?>
-            <h2>Welcome To Camagru</h2>
         </main>
         <footer>
-            <h2>Camagru 42 school project</h2>
+            <h2>Camagru is a 42 school project</h2>
             <h3>made by mfonteni</h3>
         </footer>
     </body>
