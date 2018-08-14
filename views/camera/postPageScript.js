bdaf599b0c043d03dedde9c1f866d32fdc0717c4
Razshal <fontenille.mathieu@gmail.com>
2018-08-14 15:36:46 +0200

    preview dynamic size

---
+++ views/camera/postPageScript.js
@@ -0,0 +1,58 @@
+window.onload = async () => {
+
+    const captureButton = document.getElementById('captureButton');
+    const video = document.getElementById('video');
+    const userFile = document.getElementById("userFile");
+    const errorPlace = document.getElementById("errorPlace");
+    const cameraPlace = document.getElementById('cameraPlace');
+    const canvas = document.getElementById('canvas');
+
+    /************** Camera Stream **************/
+
+    if (navigator.mediaDevices === undefined)
+        navigator.mediaDevices = {};
+    if (navigator.mediaDevices.getUserMedia === undefined) {
+        navigator.mediaDevices.getUserMedia = constraints => {
+            let getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+            if (!getUserMedia)
+                return Promise.reject(new Error('Cannot use camera with your browser'));
+            return new Promise(function(resolve, reject) {
+                getUserMedia.call(navigator, constraints, resolve, reject);
+            });
+        }
+    }
+    navigator.mediaDevices.getUserMedia({ audio: false, video: { width: 1280, height: 720 } })
+        .then(function(stream) {
+            if ("srcObject" in video)
+                video.srcObject = stream;
+            else
+                video.src = window.URL.createObjectURL(stream);
+            video.onloadedmetadata = video.play();
+        })
+        .catch(function(error) {
+            errorPlace.style.display = "block";
+            video.style.display = "none";
+            if (error.name === "NotAllowedError")
+                errorPlace.innerHTML = "You refused to share your camera";
+            else
+                errorPlace.innerHTML = error.name + ": " + error.message;
+            errorPlace.classList.add("error");
+        });
+
+    /************** Camera Caption **************/
+
+    captureButton.onclick = () => {
+        canvas.style.display= 'block';
+        canvas.width = video.width;
+        canvas.height = video.height;
+        canvas.getContext('2d').drawImage(video, 0, 0,
+            video.width * (video.width / 640), video.height * (video.height / 360));
+    };
+
+    /************** Load User Picture **************/
+
+    userFile.onchange = () => {
+        canvas.getContext('2d').drawImage(this.files[0], 0, 0);
+
+    };
+};
--- views/post.php
+++ views/post.php
@@ -1,63 +1,14 @@
 <?php ob_start(); ?>
-<script>
-    window.onload = async () => {
-        const captureButton = document.getElementById('captureButton');
-        const canvas = document.getElementById('canvas');
-        const video = document.getElementById('video');
 
-        /************** Camera Stream **************/
-
-        if (navigator.mediaDevices === undefined)
-            navigator.mediaDevices = {};
-        if (navigator.mediaDevices.getUserMedia === undefined) {
-            navigator.mediaDevices.getUserMedia = constraints => {
-                let getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-                if (!getUserMedia)
-                    return Promise.reject(new Error('Cannot use camera with your browser'));
-                return new Promise(function(resolve, reject) {
-                    getUserMedia.call(navigator, constraints, resolve, reject);
-                });
-            }
-        }
-        navigator.mediaDevices.getUserMedia({ audio: false, video: true })
-            .then(function(stream) {
-                if ("srcObject" in video)
-                    video.srcObject = stream;
-                else
-                    video.src = window.URL.createObjectURL(stream);
-                video.onloadedmetadata = video.play();
-            })
-            .catch(function(error) {
-                let errorPlace = document.getElementById("errorPlace");
-                errorPlace.style.display = "block";
-                video.style.display = "none";
-                errorPlace.innerHTML = error.name + ": " + error.message;
-                errorPlace.classList.add("error");
-            });
-
-        /************** Camera Caption **************/
-
-            captureButton.onclick = () => {
-                canvas.style.display = 'block';
-                canvas.getContext('2d').drawImage(video, 0, 0);
-            }
-        }
-</script>
-
-<section id="cameraArea" class="container">
-    <video autoplay id="video" width="400" height="400"></video>
-    <button id="captureButton">Snap</button>
-</section>
-
-<form class="loginForm" method="post" enctype="multipart/form-data">
-    <canvas id="canvas" width="400" height="400" style="display: none;"></canvas>
-    Or
-    <div>
-        Send from your computer<br/>
-        <input type="file" name="fileToUpload" id="fileToUpload"/>
-    </div>
-    <input type="text" name="title" placeholder="Post title"/>
-    <textarea placeholder="Post description" name="text"></textarea>
-</form>
+<script src="views/camera/postPageScript.js"></script>
+<div id="cameraPlace">
+    <video autoplay id="video" width="640" height="360"></video>
+    <canvas id="canvas" style="display: none"></canvas>
+</div>
+<button id="captureButton">Snap</button>
+Or send from your computer<br/>
+<input type="file" accept="image/*" name="userFile" id="userFile"/>
+<input type="text" name="title" placeholder="Post title"/>
+<textarea placeholder="Post description" name="text"></textarea>
 
 <?php $content = ob_get_clean(); ?>
